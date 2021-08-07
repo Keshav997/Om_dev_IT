9b4e0a311788697f8b8b22a9c21bdba0ea03f98b
Kausik Santra <santrakausik2016@gmail.com>
2021-08-07 15:50:56 +0530

    UI-product

---
+++ hariom-ui/src/components/category/categary.js
@@ -0,0 +1,137 @@
+import React from "react";
+export default function Categary() {
+  return (
+    <>
+      <div className="flex flex-row overflow-auto md:overflow-scroll">
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              Todays Offer
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              Fashion
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              Mobile & Tablets
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              Beauty, Food, Baby Care
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              Home & Furniture
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              Sports
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              Chokolets
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              Electronics & Accessories
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              All
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              All
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              All
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              All
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              All
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              All
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              All
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col space-x-2">
+          <div className="w-20 h-20 rounded-full flex items-center p-2 mb-2 border-4 border-secondary shadow-lg"></div>
+          <div className="w-20">
+            <span className="relative flex flex-row items-center justify-center cursor-pointer">
+              All
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ hariom-ui/src/components/product/product.js
@@ -0,0 +1,27 @@
+import React from "react";
+export default function Product() {
+  return (
+    <>
+      <div className="grid grid-cols-6 gap-x-2 gap-y-2 pt-2 bg-white h-60 w-full">
+        <div className="col-span-3 h-60 md:col-span-1 border-8 border-secondary flex items-center justify-center">
+          Hallo
+        </div>
+        <div className="col-span-3 h-60 md:col-span-1 border-8 border-secondary flex items-center justify-center">
+          Hallo
+        </div>
+        <div className="col-span-3 h-60 md:col-span-1 border-8 border-secondary flex items-center justify-center">
+          Hallo
+        </div>
+        <div className="col-span-3 h-60 md:col-span-1 border-8 border-secondary flex items-center justify-center">
+          Hallo
+        </div>
+        <div className="col-span-3 h-60 md:col-span-1 border-8 border-secondary flex items-center justify-center">
+          Hallo
+        </div>
+        <div className="col-span-3 h-60 md:col-span-1 border-8 border-secondary flex items-center justify-center">
+          Hallo
+        </div>
+      </div>
+    </>
+  );
+}
